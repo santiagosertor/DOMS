fc5ce6397fc3ccc26024a01cd3f2829dd76b2a96
santiago <santiagomutis2005@gmail.com>
2025-05-08 21:30:43 -0500

    casi finaliza

--- eventos/app.js
+++ eventos/app.js
@@ -1,5 +1,5 @@
-import { valido } from "./modulos";
-valido;
+import {esValido} from "./modulo.js";
+//Variables
 const formulario = document.querySelector("form");
 const nombre = document.querySelector("[name=nombre]");
 const apellido = document.querySelector("[name=apellido]");
@@ -8,146 +8,99 @@ const documento = document.querySelector("[name=documento]");
 const usuario = document.querySelector("[name=usuario]");
 const contrasena = document.querySelector("[name=contrasena]");
 const politicas = document.querySelector("[name=politicas]");
-const boton = document.querySelector("[name=btn_validar]");
-// const usuarios=document.querySelector("")
+const boton = document.querySelector("#btn_validar");
 
+const usuarios = document.querySelector("#usuarios");
+
+//Funciones
 const validar = (event) => {
-  if (nombre.value === "") {
-    if (nombre.nextElementSibling) {
-      nombre.nextElementSibling.remove();
+  const valores = [nombre, apellido, telefono, documento, usuario, contrasena];
+  const valoresString = [
+    "nombre",
+    "apellido",
+    "telefono",
+    "documento",
+    "usuario",
+    "contraseña",
+  ];
+  event.preventDefault();
+  let cont = 0;
+  let focus = 0;
+  let mensaje = "Ingrese correctamente: ";
+  for (let x = 0; x < valores.length; x++) {
+    if (valores[x].value.trim() === "")
+    {
+    if(valores[x].nextElementSibling) valores[x].nextElementSibling.remove();
+    if (cont > 0) {
+      mensaje += ", " + valoresString[x];
+    } else {
+      mensaje += " " + valoresString[x];
+      cont++;
     }
-    nombre.focus();
-    nombre.style.border = "1px solid red";
-    nombre.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de nombre es obligatorio";
-    span.classList.add("error");
-    nombre.insertAdjacentElement("afterend", span);
-    event.preventDefault();
-  }
-
-  if (apellido.value === "") {
-    if (apellido.nextElementSibling) {
-      apellido.nextElementSibling.remove();
+    valores[x].style.border = "2px solid red";
+    if (focus == 0) {
+      valores[x].focus();
+      focus++;
     }
-    apellido.focus();
-    apellido.style.border = "1px solid red";
-    apellido.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de apellido es obligatorio";
-    span.classList.add("error");
-    apellido.insertAdjacentElement("afterend", span);
-    event.preventDefault();
-  }
-
-  if (telefono.value === "") {
-    if (telefono.nextElementSibling) {
-      telefono.nextElementSibling.remove();
+    let aviso = document.createElement("span");
+    aviso.classList.add("avisoError");
+    aviso.textContent = `El campo ${valoresString[x]} es Obligatorio`;
+    valores[x].insertAdjacentElement("afterend", aviso);
     }
-    telefono.focus();
-    telefono.style.border = "1px solid red";
-    telefono.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de telefono es obligatorio";
-    span.classList.add("error");
-    telefono.insertAdjacentElement("afterend", span);
-    event.preventDefault();
+    // else formulario.addEventListener("submit", esValido);
   }
-
-  if (documento.value === "") {
-    if (documento.nextElementSibling) {
-      documento.nextElementSibling.remove();
-    }
-    documento.focus();
-    documento.style.border = "1px solid red";
-    documento.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de documento es obligatorio";
-    span.classList.add("error");
-    documento.insertAdjacentElement("afterend", span);
-    event.preventDefault();
-  }
-
-  if (usuario.value === "") {
-    if (usuario.nextElementSibling) {
-      usuario.nextElementSibling.remove();
-    }
-    usuario.focus();
-    usuario.style.border = "1px solid red";
-    usuario.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de usuario es obligatorio";
-    span.classList.add("error");
-    usuario.insertAdjacentElement("afterend", span);
-    event.preventDefault();
-  }
-
-  if (contrasena.value === "") {
-    if (contrasena.nextElementSibling) {
-      contrasena.nextElementSibling.remove();
-    }
-    contrasena.focus();
-    contrasena.style.border = "1px solid red";
-    contrasena.classList.add("error");
-    const span = document.createElement("span");
-    span.textContent = "El campo de contraseña es obligatorio";
-    span.classList.add("error");
-    contrasena.insertAdjacentElement("afterend", span);
-    event.preventDefault();
-  }
+  if (mensaje != "Ingrese correctamente: ") alert(mensaje);
 };
-
 const letras = (event) => {
-  const regexp = /^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]$/;
-  if (
-    !regexp.test(event.key) &&
-    event.key !== "Backspace" &&
-    event.key !== "Tab"
-  ) {
+  const regexp = /^[a-zA-Z]$/;
+  if (!regexp.test(event.key) && event.key !== "Backspace" && event.key !== "Tab") {
     event.preventDefault();
   }
+    if (event.target.value.length >= 10) {
+      event.preventDefault();
+  }
 };
-
 const numeros = (event) => {
-  if (
-    !/^\d$/.test(event.key) &&
-    event.key !== "Backspace" &&
-    event.key !== "Tab"
-  ) {
+  if (!/^\d$/.test(event.key) && event.key !== "Backspace" && event.key !== "Tab") {
     event.preventDefault();
   }
-  if (event.target.value.length >= 10 && event.key !== "Backspace") {
+  if (event.target.value.length >= 10) {
     event.preventDefault();
   }
 };
-
 const limpiar = (event) => {
-  if (event.target.value != "") {
-    event.target.classList.remove("error");
+  if (event.target.value !== "") {
+    event.target.classList.remove("error")
+    event.target.style.border = "0px";
     if (event.target.nextElementSibling) {
       event.target.nextElementSibling.remove();
     }
   }
-};
-
-const acepta = (e) => {
+}
+const acepta = () => {
   if (!politicas.checked) {
     boton.setAttribute("disabled", "");
-  } else {
-    boton.removeAttribute("disabled");
   }
-};
-
-addEventListener("DOMContentLoaded", acepta);
-politicas.addEventListener("change", acepta);
-formulario.addEventListener("submit", validar);
-nombre.addEventListener("keydown", letras);
-apellido.addEventListener("keydown", letras);
-telefono.addEventListener("keydown", numeros);
-documento.addEventListener("keydown", numeros);
-nombre.addEventListener("blur", limpiar);
-apellido.addEventListener("blur", limpiar);
-telefono.addEventListener("blur", limpiar);
-documento.addEventListener("blur", limpiar);
-usuario.addEventListener("blur", limpiar);
-contrasena.addEventListener("blur", limpiar);
+  else {
+    boton.removeAttribute('disabled');
+  }
+}
+const isValid = (e) => {
+  let data = esValido(e)
+  console.log(data);
+}
+//Eventos
+addEventListener("DOMContentLoaded",acepta);
+politicas.addEventListener('change',acepta)
+formulario.addEventListener("submit", isValid);
+// formulario.addEventListener("submit", validar);
+// nombre.addEventListener("keydown", letras);
+// apellido.addEventListener("keydown", letras);
+// telefono.addEventListener("keydown", numeros);
+// documento.addEventListener("keydown", numeros);
+// nombre.addEventListener("blur", limpiar);
+// apellido.addEventListener("blur", limpiar);
+// telefono.addEventListener("blur", limpiar);
+// documento.addEventListener("blur", limpiar);
+// usuario.addEventListener("blur", limpiar);
+// contrasena.addEventListener("blur", limpiar);
--- eventos/modulos.js
+++ eventos/modulos.js
@@ -1,25 +1,52 @@
-export const valido = (e) =>{
-  e.preventDefaul();
-  console.log(e.target.childrean);
+export const esValido = (e) => {
+  e.preventDefault();
+  // console.log(e.target.children);
+  const obj = {};
   const campos = [...e.target].filter((elemento) => {
-    return elemento.hasAttribute("required")
-  })
+    return elemento.hasAttribute("required");
+  });
+  const radios = [...campos].filter((elemento) => {
+    return elemento.type === "radio";
+  });
+  const checkbox = [...campos].filter((elemento) => {
+    return elemento.type === "checkbox";
+  });
+  const campo_radio = radios.find((radio) => radio.checked) || [];
+  if (campo_radio.length === 0) {
+    obj[radios[0].name] = "";
+  } else {
+    obj[radios[0].name] = campo_radio.value;
+  }
+  const campo_checkbox = checkbox.filter((e) => e.checked);
+  if (campo_checkbox.length < 3) {
+    obj[checkbox[0].name] = "";
+  } else {
+    obj[checkbox[0].name] = [...campo_checkbox].map(e => e.value);
+  }
 
-  // recorremos los elementos
-  campos.forEach(campos => {
-    switch (campos.tagName) {
-      case'input':
-        console.log(campos.type);
-        
+  campos.forEach((campo) => {
+    switch (campo.tagName){
+      case "INPUT":
+        if (campo.type == "text" || campo.type == "number" || campo.type == "password" || campo.type == "tel") {
+          obj[campo.name] = campo.value;
+          if (campo.value.trim() === "") {
+            campo.classList.add("error");
+          }
+        }
+        break;
+      case "SELECT":
+        obj[campo.name] = campo.selectedIndex;
+        if (campo.selectedIndex === 0) {
+          campo.classList.add("error");
+        }
+      default:
         break;
     }
-
-    
-    if (campos.value ==='') {
-      campos.classList.add("error")
-    }
-     
-     
-  })
-}
-
+  });
+  return obj;
+};
+//usuario
+//ciudades
+//generos
+//lenguajes
+//todo unido(usuario y lenguajes)
